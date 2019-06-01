6a4ea44f10d5e694a12f32e8b76c654d45709331
f1r3starter <andrey.filenochko@gmail.com>
2019-06-01 19:48:24 +0300

    added validators

--- lesson2/task1/index.js
+++ lesson2/task1/index.js
@@ -1,10 +1,30 @@
 class TimersManager {
     constructor() {
         this.timers = [];
+        this.timerValidator = [
+            ['name', name => typeof name === 'string', 'Timer name should be a string'],
+            ['name', name => name !== '', 'Timer name could not be empty'],
+            ['name', name => !this.timers.some(timer => timer.name === name), 'Timer with the same name already exists'],
+            ['delay', delay => typeof delay === 'number' , 'Timer delay should be a number'],
+            ['delay', delay => delay > 0, 'Timer delay could not be less than 0 ms'],
+            ['delay', delay => delay < 5000, 'Timer delay could not be bigger than 5000 ms'],
+            ['interval', interval => typeof interval === 'boolean', 'Timer interval should be boolean'],
+            ['job', job => typeof job === 'function', 'Timer job should be a function'],
+        ]
     }
 
-    add() {
+    add(timer, ...timerArgs) {
+        let errors = this.timerValidator
+            .filter(([name, validator]) => !validator(timer[name]))
+            .map(([, , error]) => error)
+            .join("\n");
+        if (errors) {
+            throw new Error(`Something went wrong while adding new timer, please, check next errors:\n ${errors}`);
+        }
 
+        this.timers.push({timerArgs, ...timer});
+
+        return this;
     }
 
     remove() {
@@ -25,20 +45,5 @@ class TimersManager {
 
     resume() {
 
-    }
-
-    validate(timer) {
-        if (typeof timer.name !== 'string') {
-            throw Error ('timer name should be string')
-        }
-        if (typeof timer.delay !== 'number') {
-            throw Error ('timer delay should be number')
-        }
-        if (typeof timer.interval !== 'boolean') {
-            throw Error ('timer interval should be boolean')
-        }
-        if (typeof timer.job !== 'function') {
-            throw Error ('timer job should be function')
-        }
     }
 }
