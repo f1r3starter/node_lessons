4ce5eb9923f43ae175908a9b580ce66a27c13555
f1r3starter <andrey.filenochko@gmail.com>
2019-06-02 12:36:19 +0300

    timer manager methods implementation

--- lesson2/task1/index.js
+++ lesson2/task1/index.js
@@ -14,7 +14,7 @@ class TimersManager {
     }
 
     add(timer, ...timerArgs) {
-        let errors = this.timerValidator
+        const errors = this.timerValidator
             .filter(([name, validator]) => !validator(timer[name]))
             .map(([, , error]) => error)
             .join("\n");
@@ -27,24 +27,45 @@ class TimersManager {
         return this;
     }
 
-    remove() {
-
+    remove(timerName) {
+        this.pause(timerName);
+        this.timers = this.timers.filter(timer => timer.name !== timerName);
     }
 
     start() {
+        const startedTimers = this.timers.some(timer => timer.hasOwnProperty('id'));
+        if (startedTimers) {
+            throw new Error('There are already started timers');
+        }
 
+        this.timers = this.timers.map(timer => this.resume(timer.name));
     }
 
     stop() {
-
+        this.timers = this.timers.map(timer => this.pause(timer.name));
     }
 
-    pause() {
+    pause(timerName) {
+        const timer = this._findTimer(timerName);
+        timer.interval ? clearInterval(timer.id) : clearTimeout(timer.id);
+        delete timer.id;
+
+        return timer;
+    }
+
+    resume(timerName) {
+        const timer = this._findTimer(timerName);
+        const timerId = timer.interval ? setInterval(timer.job, timer.delay, ...timer.timerArgs) : setTimeout(timer.job, timer.delay, ...timer.timerArgs);
 
         return {id: timerId, ...timer};
     }
 
-    resume() {
+    _findTimer(timerName) {
+        const timer = this.timers.find(timer => timer.name === timerName);
+        if (!timer) {
+            throw new Error('Timer does not exists');
+        }
 
+        return timer;
     }
 }
