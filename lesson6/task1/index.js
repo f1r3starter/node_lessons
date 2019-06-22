4c062f933ca15cfdd116d9d4440ee8dad8376e14
f1r3starter <andrey.filenochko@gmail.com>
2019-06-22 20:51:41 +0300

    json 2 csv converter

--- lesson6/task1/index.js
+++ lesson6/task1/index.js
@@ -1,32 +1,38 @@
 const fs = require('fs');
 const path = require('path');
 const { Transform } = require('stream');
+const endOfLine = require('os').EOL;
 
 class Json2csv extends Transform {
     constructor(delimiter = ';', options = {}) {
         super(options);
         this.delimiter = delimiter;
+        this.prevPart = '';
     }
 
     _transform(chunk, encoding, done) {
-        const customerData = JSON.parse(chunk.toString());
-        const validationError =  this.validator.isInvalid(customerData);
-        if (validationError) {
-            this.emit('error', validationError);
-        }
+        const lines = this._parseChunk(chunk.toString());
+        lines.map(line => this._convertToCsv(line)).forEach(csvLine => this.push(csvLine));
 
-        this.push(JSON.stringify(this._decryptObject(customerData)));
         done();
     }
 
+    _parseChunk(chunk) {
+        const prefix = chunk.substr(0,1) === '[' ? '' : '[' + this.prevPart;
+        const lastBrace = chunk.lastIndexOf('}') + 1;
+        const jsonData = prefix + chunk.substr(0, lastBrace) + ']';
+        this.prevPart = chunk.substr(lastBrace + 1);
+        return JSON.parse(jsonData);
+    }
+
     _convertToCsv(jsonData) {
-        return jsonData.values().join(this.delimiter);
+        return Object.values(jsonData).map(data => JSON.stringify(data)).join(this.delimiter) + endOfLine;
     }
 }
 
-const fileStream = fs.createReadStream(path.join(__dirname, '/data/test.txt'));
+const fileStream = fs.createReadStream(path.join(__dirname, '/data/comments.json'));
 const fileWriteStream = fs.createWriteStream(
-    path.join(__dirname, '/data/test.txt.copy')
+    path.join(__dirname, '/data/comments.csv')
 );
 
 const json2csv = new Json2csv();
