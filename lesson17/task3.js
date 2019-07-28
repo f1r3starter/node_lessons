0b9c45ac3746f37f588194b81615c6a7fcf6589b
f1r3starter <andrey.filenochko@gmail.com>
2019-07-28 16:17:35 +0300

    task3

---
+++ lesson17/task3.js
@@ -0,0 +1,59 @@
+const size = 3
+const pages = Math.ceil(db.customers.countDocuments({}) / size); // 3.1 // 4
+
+for (let page = 0; page < pages; page++) {
+    const cursor = db.orders.aggregate([
+        {
+            $group:
+                {
+                    _id: {
+                        product: '$product',
+                        customerId: '$customerId'
+                    },
+                    total: {
+                        $sum: 1
+                    }
+                }
+        },
+        {
+            '$lookup': {
+                'from': 'customers',
+                'localField': '_id.customerId',
+                'foreignField': '_id',
+                'as': 'customer'
+            }
+        },
+        {
+            $group:
+                {
+                    _id: {
+                        customerId: '$_id.customerId'
+                    },
+                    fName: { $first: '$customer.name.first' },
+                    lName: { $first: '$customer.name.last' },
+                    orders: {
+                        $push: {
+                            '_id': '$_id.product',
+                            'total': '$total'
+                        },
+                    }
+                }
+        },
+        {
+            $sort: {
+                '_id.customerId': 1
+            }
+        },
+        {
+            '$project': {
+                '_id': false
+            }
+        },
+        { '$facet'    : {
+                metadata: [ { $count: "total" }, { $addFields: { page: page + 1 } } ],
+                data: [ { $skip: page * size }, { $limit: size } ] // add projection here wish you re-shape the docs
+            } }
+    ]);
+
+    print(tojson(cursor.toArray()));
+}
