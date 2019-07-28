42cfffe144ad90f628e686142a5339bd3578d8f7
f1r3starter <andrey.filenochko@gmail.com>
2019-07-28 18:11:40 +0300

    task2

---
+++ lesson18/task2.js
@@ -0,0 +1,1 @@
+db.customers.createIndex({ nickname: 1, email: 1 }, { unique: true })
---
+++ lesson18/task3.js
@@ -0,0 +1,1 @@
+db.customers.createIndex({ 'name.first': 'text', 'name.last': 'text', email: 'text', nickname: 'text' })
